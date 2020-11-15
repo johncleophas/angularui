d9b9c1645448170927fc1284d0353b3ace0813dc
John Cleophas <JCleophas@oldmutual.com>
2020-11-15 11:05:55 +0200

    1.) added routes and components

--- angularuidemo/src/app/app-routing.module.ts
+++ angularuidemo/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const userModule = () => import('./modules/user/user.module').then(x => x.UserModule);
+const homeModule = () => import('./modules/home/home.module').then(x => x.HomeModule);
+const subscriptionModule = () => import('./modules/subscription/subscription.module').then(x => x.SubscriptionModule);
+
+const routes: Routes = [
+  { path: '', loadChildren: homeModule},
+  { path: 'user', loadChildren: userModule },
+  { path: 'subscription', loadChildren: subscriptionModule }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ angularuidemo/src/app/modules/subscription/subscription.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SubscriptionComponent } from './components/subscription/subscription.component';
+import { SubscriptionRoutingModule } from './subscription-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    SubscriptionComponent
+  ],
+  imports: [
+    CommonModule,
+    SubscriptionRoutingModule
+  ]
+})
+export class SubscriptionModule { }
